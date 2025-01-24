bd8329da04980dc451b2d8021ec2ed21010d849e
maija <maijaaaw@gmail.com>
2025-01-24 20:29:24 +0200

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,36 @@ export const enum Scene {
 
 function App() {
    const [count, setCount] = useState(0);
-   const [scene, setScene] = useState<Scene>(Scene.StartMenu)
+   const [scene, setScene] = useState<Scene>(Scene.StartMenu);
 
-   const toggleScene = () => setScene(scene === Scene.StartMenu ? Scene.Game : Scene.StartMenu)
+   const toggleScene = () =>
+      setScene(
+         scene === Scene.StartMenu ? Scene.Game : Scene.StartMenu,
+      );
 
    if (scene === Scene.StartMenu) {
-      return <StartMenu changeScene={toggleScene}/>
+      return <StartMenu changeScene={toggleScene} />;
    }
-const rows = 10;
-const cols = 10;
+   const rows = 10;
+   const cols = 10;
 
-const generateDivs = (rows: number, cols: number) => {
-   const grid: React.ReactNode[] = [];
-   for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-         grid.push(
-            <div className="game-tile" key={`${row} ${col}`}></div>,
-         );
+   const generateDivs = (rows: number, cols: number) => {
+      const grid: React.ReactNode[] = [];
+      for (let row = 0; row < rows; row++) {
+         for (let col = 0; col < cols; col++) {
+            grid.push(
+               <div
+                  className="game-tile"
+                  key={`${row} ${col}`}
+               ></div>,
+            );
+         }
       }
-   }
 
-   return grid;
-};
+      return grid;
+   };
 
-const divs = generateDivs(rows, cols);
+   const divs = generateDivs(rows, cols);
 
    return (
       <div className="container">
@@ -50,12 +56,13 @@ const divs = generateDivs(rows, cols);
          <h1>Vite + React</h1>
          <div className="card">
             <button onClick={() => setCount(count => count + 1)}>
-               count is {Math.random() > 0.5 ? count : Math.round(Math.random() * 10)}
+               count is{' '}
+               {Math.random() > 0.5
+                  ? count
+                  : Math.round(Math.random() * 10)}
             </button>
             <p>Tsumonjää</p>
-            <button onClick={toggleScene}>
-               toggle scene
-            </button>
+            <button onClick={toggleScene}>toggle scene</button>
          </div>
          <p className="read-the-docs">Hähähähä</p>
       </div>
