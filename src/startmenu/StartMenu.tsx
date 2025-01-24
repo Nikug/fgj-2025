12e190f1c531d6fcca45fd53be2264c4dd0a6dfb
maija <maijaaaw@gmail.com>
2025-01-24 20:44:07 +0200

    email

--- src/startmenu/StartMenu.tsx
+++ src/startmenu/StartMenu.tsx
@@ -1,9 +1,18 @@
-export function StartMenu({ changeScene }: any) {
+import { useState } from 'react';
+import './startmenu.css';
+
+type StartMenuProps = {
+   changeScene: () => void;
+};
+
+export function StartMenu({ changeScene }: StartMenuProps) {
+   const [players, setPlayers] = useState<string[]>([]);
    return (
       <div className="start-menu">
          <div className="star-name__title">BUBBLE BABBLE</div>
-
          <button onClick={changeScene}>toggle scene</button>
+
+         <div className="start-menu__player-list"></div>
       </div>
    );
 }
