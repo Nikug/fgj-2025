9e1b036ecc5c4de2551ed3d6e0300ce7d687d688
Niku Grönberg <gronbergniku@gmail.com>
2025-01-24 21:05:50 +0200

    add player and movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import './App.css';
+import { V2 } from './types';
+import { Player } from './Player';
 
-const rows = 10;
-const cols = 10;
-
-const generateDivs = (rows: number, cols: number) => {
-   const grid: React.ReactNode[] = [];
-   for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-         grid.push(
-            <div className="game-tile" key={`${row} ${col}`}></div>,
-         );
-      }
-   }
-
-   return grid;
-};
-
-const divs = generateDivs(rows, cols);
+export const rows = 10;
+export const cols = 10;
 
 function App() {
+   const [playerPosition, setPlayerPosition] = useState<V2>({
+      x: 5,
+      y: 5,
+   });
+
+   const generateDivs = () => {
+      const grid: React.ReactNode[] = [];
+      for (let row = 0; row < rows; row++) {
+         for (let col = 0; col < cols; col++) {
+            const drawPlayer =
+               row === playerPosition.y && col === playerPosition.x;
+            grid.push(
+               <div className="game-tile" key={`${row} ${col}`}>
+                  {drawPlayer && (
+                     <Player
+                        position={playerPosition}
+                        setPosition={setPlayerPosition}
+                     />
+                  )}
+               </div>,
+            );
+         }
+      }
+
+      return grid;
+   };
+
+   const divs = generateDivs();
+
    return (
       <div className="container">
          <div className="game-container">
