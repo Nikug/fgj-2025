961d66448c88e358702cec0d1e1fae90f25c4609
vilperi <p-makela@outlook.com>
2025-01-24 17:32:56 +0200

    maija on tyhäm

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Maija on tyhäm
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Hähähähä
       </p>
     </>
   )
