b20b307a0f15ddc1385a23e82e440e5a2341294e
vilperi <p-makela@outlook.com>
2025-01-24 21:18:26 +0200

    vilperi css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './vilperi.css';
 import { Router } from './Vilperi.tsx';
 
 createRoot(document.getElementById('root')!).render(
