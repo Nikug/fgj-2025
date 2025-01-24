033002f9980d817d2828c7974fb1c556a390bd19
vilperi <p-makela@outlook.com>
2025-01-24 23:32:22 +0200

    remove random ehx

--- src/Vilperi.tsx
+++ src/Vilperi.tsx
@@ -202,8 +202,3 @@ const VilperiBox = (props: VilperiBoxProps) => {
       </div>
    );
 };
-
-const randomHexColorCode = () => {
-   let n = (Math.random() * 0xfffff * 1000000).toString(16);
-   return '#' + n.slice(0, 6);
-};
