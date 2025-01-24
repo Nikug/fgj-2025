9bce5429c70a3b9192401f93c797e5ede0d94848
Niku Grönberg <gronbergniku@gmail.com>
2025-01-24 20:45:17 +0200

    add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-   const [count, setCount] = useState(0);
+const rows = 10;
+const cols = 10;
 
+const generateDivs = (rows: number, cols: number) => {
+   const grid: React.ReactNode[] = [];
+   for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+         grid.push(
+            <div className="game-tile" key={`${row} ${col}`}></div>,
+         );
+      }
+   }
+
+   return grid;
+};
+
+const divs = generateDivs(rows, cols);
+
+function App() {
    return (
-      <>
-         <div>
-            <a href="https://vite.dev" target="_blank">
-               <img
-                  src={viteLogo}
-                  className="logo"
-                  alt="Vite logo"
-               />
-            </a>
-            <a href="https://react.dev" target="_blank">
-               <img
-                  src={reactLogo}
-                  className="logo react"
-                  alt="React logo"
-               />
-            </a>
+      <div className="container">
+         <div className="game-container">
+            <div
+               className="game-grid"
+               style={{
+                  gridTemplateRows: `repeat(${rows}, 1fr)`,
+                  gridTemplateColumns: `repeat(${cols}, 1fr)`,
+               }}
+            >
+               {divs}
+            </div>
          </div>
-         <h1>Vite + React</h1>
-         <div className="card">
-            <button onClick={() => setCount(count => count + 1)}>
-               count is {Math.random() > 0.5 ? count : Math.round(Math.random() * 10)}
-            </button>
-            <p>Tsumonjää</p>
-         </div>
-         <p className="read-the-docs">Hähähähä</p>
-      </>
+      </div>
    );
 }
 
