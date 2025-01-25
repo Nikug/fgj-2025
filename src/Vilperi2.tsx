8bde7a8123204b43864319ddd970cafd4e7aee84
vilperi <p-makela@outlook.com>
2025-01-25 19:32:55 +0200

    better floats

--- src/Vilperi2.tsx
+++ src/Vilperi2.tsx
@@ -5,24 +5,29 @@ import './vilperi.css';
 interface Props {
    model: PlayerModelType;
    name: string;
+   index: number;
+   dir?: 'right' | 'left';
    color?: string;
    highlight?: boolean;
 }
 
 export const FloatingPlayerBubble = (props: Props) => {
-   const { model, color, name } = props;
+   const { model, color, name, dir, index } = props;
 
    const cssVars = {
       '--player-color': color,
+      '--direction': dir === 'right' ? 'reverse' : 'normal',
+      '--top': `${index * 20 + 10}px`,
    } as React.CSSProperties;
 
    return (
-      <div className="floating-player-container">
+      <div className="floating-player-container" style={cssVars}>
          <div
             style={{
                position: 'absolute',
                width: '4em',
                height: '4em',
+
                containerType: 'inline-size',
             }}
          >
