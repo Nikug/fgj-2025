f49ac5237ef55e8e73c09e5c633c8b9bcca3d4e9
Niku Grönberg <gronbergniku@gmail.com>
2025-01-25 10:55:29 +0200

    shuffle players

--- src/states/MasterState.tsx
+++ src/states/MasterState.tsx
@@ -1,8 +1,12 @@
 import { create } from 'zustand';
-import { GamePhase, Player, V2, Action } from '../types';
+import { GamePhase, Player, V2, Action, Scene } from '../types';
 import { immer } from 'zustand/middleware/immer';
+import { shuffleList } from '../random';
 
 interface MasterState {
+   scene: Scene;
+   setScene: (scene: Scene) => void;
+
    gamePhase: GamePhase;
    setGamePhase: (phase: GamePhase) => void;
 
@@ -14,10 +18,31 @@ interface MasterState {
    playerTurn: string | null;
    setPlayerTurn: (id: string) => void;
    getPlayerTurn: () => Player | null;
+
+   playerOrder: string[];
+   setPlayerOrder: (ids: string[]) => void;
 }
 
 export const useMasterState = create<MasterState>()(
    immer((set, get) => ({
+      scene: Scene.StartMenu,
+      setScene: scene =>
+         set(state => {
+            let playerOrder: string[] = [];
+            let players: Player[] = [];
+            if (scene === Scene.Game) {
+               players = shuffleList(state.players);
+            }
+            playerOrder = players.map(e => e.id);
+            return {
+               scene,
+               playerOrder,
+               players,
+               playerTurn: playerOrder[0] ?? null,
+            };
+         }),
+      playerOrder: [],
+      setPlayerOrder: ids => set(() => ({ playerOrder: ids })),
       gamePhase: GamePhase.Planning,
       setGamePhase: phase => set(() => ({ phase })),
       players: [],
