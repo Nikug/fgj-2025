b2495b6a2c2dbe50943e4dd2d29e55576afc12ac
Apezkin <ape.zkin@gmail.com>
2025-01-25 21:39:56 +0200

    koko maailma korjattu

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ function App() {
       switch (gamePhase) {
          case GamePhase.Planning:
             instructionText =
-               'Plan your moves. Use the buttons below or use arow keys to move your bubble and b + arrow keys to shoot.';
+               'Plan your moves. Use the buttons below or use arow keys to move your bubble and space + arrow keys to shoot.';
             break;
          case GamePhase.Action:
             instructionText = 'Watch the action!';
