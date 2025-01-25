4a702e0fa24ef8435793d83dbf2b3abc121b62ee
Niku Grönberg <gronbergniku@gmail.com>
2025-01-25 13:18:00 +0200

    real walls

--- src/states/resolver.ts
+++ src/states/resolver.ts
@@ -40,7 +40,10 @@ export const resolver = async () => {
                useMasterState.setState(state => {
                   newPos.y -= 1;
                   if (newPos.y < 0) newPos.y = 0;
-                  if (!playerOverlap(newPos, state.players)) {
+                  if (
+                     !playerOverlap(newPos, state.players) &&
+                     !state.hasObstacle(newPos)
+                  ) {
                      state.players[playerIndex].pos = newPos;
                   }
                });
@@ -49,7 +52,10 @@ export const resolver = async () => {
                useMasterState.setState(state => {
                   newPos.y += 1;
                   if (newPos.y >= rows) newPos.y = rows - 1;
-                  if (!playerOverlap(newPos, state.players)) {
+                  if (
+                     !playerOverlap(newPos, state.players) &&
+                     !state.hasObstacle(newPos)
+                  ) {
                      state.players[playerIndex].pos = newPos;
                   }
                });
@@ -58,7 +64,10 @@ export const resolver = async () => {
                useMasterState.setState(state => {
                   newPos.x -= 1;
                   if (newPos.x < 0) newPos.x = 0;
-                  if (!playerOverlap(newPos, state.players)) {
+                  if (
+                     !playerOverlap(newPos, state.players) &&
+                     !state.hasObstacle(newPos)
+                  ) {
                      state.players[playerIndex].pos = newPos;
                   }
                });
@@ -67,15 +76,18 @@ export const resolver = async () => {
                useMasterState.setState(state => {
                   newPos.x += 1;
                   if (newPos.x >= cols) newPos.x = cols - 1;
-                  if (!playerOverlap(newPos, state.players)) {
+                  if (
+                     !playerOverlap(newPos, state.players) &&
+                     !state.hasObstacle(newPos)
+                  ) {
                      state.players[playerIndex].pos = newPos;
                   }
                });
                break;
             case Action.Attack:
                useMasterState.setState(state => {
-                  state.createAttaaak(player.pos, 'ltr')
-               })
+                  state.createAttaaak(player.pos, 'ltr');
+               });
                break;
             default:
                window.alert('what');
